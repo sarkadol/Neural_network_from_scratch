5c127032a09164818ad06c03355e577e090ac86c
sarkadol <sarka.dolezalova123@gmail.com>
2024-12-06 11:26:00 +0100

    run javac optimalizators

--- run.sh
+++ run.sh
@@ -14,7 +14,15 @@ echo "#################"
 # g++ -Wall -std=c++17 -O3 src/main.cpp src/file2.cpp -o network
 rm -rf out/*
 
-javac -d out -sourcepath src $(find src -name "*.java")
+#javac -d out -sourcepath src $(find src -name "*.java")
+javac -J-Xmx4G -J-XX:+UnlockExperimentalVMOptions -J-XX:+UseParallelGC -d out -sourcepath src $(find src -name "*.java")
+# javac: Java compiler to compile .java files into .class files.
+# -J-Xmx4G: Allocates up to 4 GB heap memory for the compiler's JVM.
+# -J-XX:+UnlockExperimentalVMOptions: Enables experimental JVM options.
+# -J-XX:+UseParallelGC: Uses Parallel Garbage Collector for faster compilation.
+# -d out: Outputs compiled .class files into the "out" directory.
+# -sourcepath src: Specifies the "src" directory for source files.
+# $(find src -name "*.java"): Finds all .java files in the "src" directory.
 
 if [ $? -ne 0 ]; then
   echo "Compilation failed!"
